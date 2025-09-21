a008c5656286d2e670156046b9ed0c34780a363b
ebudae2 <ebudae2@PC>
2025-09-21 14:50:05 +0200

    created neccesary things

---
+++ lakeview/Source/PluginEditor.cpp
@@ -0,0 +1,38 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+LakeviewAudioProcessorEditor::LakeviewAudioProcessorEditor (LakeviewAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    setSize (400, 300);
+}
+
+LakeviewAudioProcessorEditor::~LakeviewAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void LakeviewAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (juce::FontOptions (15.0f));
+    g.drawFittedText ("", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void LakeviewAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
